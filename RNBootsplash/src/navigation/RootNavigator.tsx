88aa8dda2b561301cb1a0f5195ce43e69e8429b3
Nihar Rupareliya <nhr199718@gmail.com>
2023-09-15 23:22:08 +1000

    bootsplash example with react navigation

--- RNBootsplash/src/navigation/RootNavigator.tsx
+++ RNBootsplash/src/navigation/RootNavigator.tsx
@@ -1,11 +1,47 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {createDrawerNavigator} from '@react-navigation/drawer';
+// import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import BootSplash from 'react-native-bootsplash';
 import HomeScreen from '../screens/HomeScreen';
 import DetailScreen from '../screens/DetailScreen';
+import ContactScreen from '../screens/ContactScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+// const Drawer = createDrawerNavigator();
+
+const usingStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailScreen} />
+      <Stack.Screen name="Contact" component={ContactScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const usingTab = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Details" component={DetailScreen} />
+      <Tab.Screen name="Contact" component={ContactScreen} />
+    </Tab.Navigator>
+  );
+};
+
+// const usingDrawer = () => {
+//   return (
+//     <Drawer.Navigator initialRouteName="Home">
+//       <Drawer.Screen name="Home" component={HomeScreen} />
+//       <Drawer.Screen name="Details" component={DetailScreen} />
+//       <Drawer.Screen name="Contact" component={ContactScreen} />
+//     </Drawer.Navigator>
+//   );
+// };
 
 const RootNavigator = () => {
   return (
@@ -14,10 +50,7 @@ const RootNavigator = () => {
         console.log('App is ready');
         BootSplash.hide({fade: true});
       }}>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailScreen} />
-      </Stack.Navigator>
+      {usingTab()}
     </NavigationContainer>
   );
 };
